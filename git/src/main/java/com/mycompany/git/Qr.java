c93d28e25a838912e23508fae77fd899acdb5bae
tar.gergo <tar.gergo21@gmail.com>
2023-02-23 14:10:06 +0100

    qr kód újracsinálása

--- git/src/main/java/com/mycompany/git/Qr.java
+++ git/src/main/java/com/mycompany/git/Qr.java
@@ -2,82 +2,29 @@ package com.mycompany.git;
 
 public class Qr {
 
-    private static int rSz = 0;
-
     public static void main(String[] args) {
-        blokkok();
-
-    }
-
-    private static void blokkok() {
-        eq();
-        eq(9);
-        eq(9,true);
-        eq(10);
-        eq();
         eq();
     }
 
     private static void eq() {
-        int min = 3;
-        int max = 12;
-        rSz = (int) (Math.random() * (max - min + 1) + min);
-        int index = 0;
-        while (index < rSz) {
-            System.out.printf("*" + " ");
-            index++;
-        }
-        System.out.println();
-
-    }
-
-    private static void eq(int sorszam) {
-        int index = 0;
-        while (index < sorszam) {
-            System.out.printf("*" + " ");
-            index++;
-        }
-        System.out.println();
-    }
-
-    private static void eq(boolean szamolas) {
-
-        int min = 3;
-        int max = 12;
-        rSz = (int) (Math.random() * (max - min + 1) + min);
-        int index = 0;
-        while (index < rSz) {
-            System.out.printf("*" + " ");
-            index++;
-        }
-               if (szamolas == true){
-            System.out.println("(" + rSz + ")");
-    }
-        else{
-            System.out.println("");
+        int min = 1;
+        int max = 10;
+        int randnom1, random2;
+        int sorok = 0;
+        while (sorok < 5) {
+            int index = 0;
+            while (index < 20) {
+                randnom1 = (int) (Math.random() * (max - min + 1) + min);
+                random2 = (int) (Math.random() * ((max + 10) - min + 1) + (min + 10));
+                if (index == randnom1 || index == random2) {
+                    System.out.printf(".");
+                } else {
+                    System.out.printf("*");
+                }
+                index++;
+            }
+            sorok++;
+            System.out.println();
         }
     }
-
-    private static void eq(int sorszam, boolean szamolas) {
-
-        int index = 0;
-        while (index < sorszam) {
-            System.out.printf("*" + " ");
-            index++;
-        }
-
-        if (szamolas == true){
-            System.out.println("""
-                               
-                               .....
-                               .   .
-                               . . .
-                               .   .
-                               .....""");
-    }
-        else{
-            System.out.println("");
-        }
-   }
-
 }
